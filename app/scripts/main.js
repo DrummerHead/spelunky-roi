a62cd3f40de792ae5d6acefefe5e506275c5ade5
DrummerHead <drummerhead@gmail.com>
2015-12-02 19:11:56 -0200

    Add Consumable, inherit from Item base class, basic styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,10 @@
 
 var config = require('config');
 var Treasure = require('treasure');
+var Consumable = require('Consumable');
+var Accountant = require('accountant');
+
+var accountant = new Accountant();
 
 
 var goldBar = new Treasure({
@@ -10,9 +14,61 @@ var goldBar = new Treasure({
     imageUrl: 'gold-bar.png',
     initialPrice: 500
   },
+  accountant,
   config
 );
 
+var rubyLarge = new Treasure({
+    name: 'Large Ruby',
+    imageUrl: 'ruby-large.png',
+    initialPrice: 1600
+  },
+  accountant,
+  config
+);
+
+var nuggetSmall = new Treasure({
+    name: 'Small Gold Nugget',
+    imageUrl: 'nugget-small.png',
+    initialPrice: 100
+  },
+  accountant,
+  config
+);
+
+var rope = new Consumable({
+    name: 'Rope',
+    imageUrl: 'rope.png',
+    initialPrice: 1
+  },
+  accountant,
+  config
+);
+
+var bomb = new Consumable({
+    name: 'Bomb',
+    imageUrl: 'bomb.png',
+    initialPrice: 1
+  },
+  accountant,
+  config
+);
+/*
+var  = new Treasure({
+    name: '',
+    imageUrl: '.png',
+    initialPrice:
+  },
+  accountant,
+  config
+);
+*/
+
 goldBar.render();
+rubyLarge.render();
+nuggetSmall.render();
+
+bomb.render();
+rope.render();
 
 })();
