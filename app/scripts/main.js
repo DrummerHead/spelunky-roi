913f00606e39b6d9563f1d74b25b4d8d6caec4af
DrummerHead <drummerhead@gmail.com>
2015-12-02 23:07:55 -0200

    Rename js files to denote purpose, create treasure-items and initialize them

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,73 +2,42 @@
 'use strict';
 
 var config = require('config');
-var Treasure = require('treasure');
-var Consumable = require('Consumable');
-var Accountant = require('accountant');
 
-var accountant = new Accountant();
+var Treasure = require('treasure-class');
+var Consumable = require('consumable-class');
+var Accountant = require('accountant-class');
+
+var treasureItems = require('treasure-items');
+
+var accountant = new Accountant(config);
 
 
-var goldBar = new Treasure({
-    name: 'Gold Bar',
-    imageUrl: 'gold-bar.png',
-    initialPrice: 500
-  },
-  accountant,
-  config
-);
+treasureItems.forEach(function(treasureItem){
+  treasureItem.instance = new Treasure(
+    treasureItem,
+    accountant,
+    config
+  ).render();
+});
 
-var rubyLarge = new Treasure({
-    name: 'Large Ruby',
-    imageUrl: 'ruby-large.png',
-    initialPrice: 1600
-  },
-  accountant,
-  config
-);
 
-var nuggetSmall = new Treasure({
-    name: 'Small Gold Nugget',
-    imageUrl: 'nugget-small.png',
-    initialPrice: 100
-  },
-  accountant,
-  config
-);
-
-var rope = new Consumable({
+new Consumable({
     name: 'Rope',
     imageUrl: 'rope.png',
     initialPrice: 750
   },
   accountant,
   config
-);
+).render();
 
-var bomb = new Consumable({
+new Consumable({
     name: 'Bomb',
     imageUrl: 'bomb.png',
     initialPrice: 750
   },
   accountant,
   config
-);
-/*
-var  = new Treasure({
-    name: '',
-    imageUrl: '.png',
-    initialPrice:
-  },
-  accountant,
-  config
-);
-*/
+).render();
 
-goldBar.render();
-rubyLarge.render();
-nuggetSmall.render();
-
-bomb.render();
-rope.render();
 
 })();
