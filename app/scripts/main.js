2a961b9c8599966a88dcc10e81e04d559d07fd42
DrummerHead <drummerhead@gmail.com>
2015-12-07 16:35:46 -0200

    Add announcer class to handle object communication, level affects items price

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,16 +7,17 @@ var treasureItems = require('treasure-items');
 var Treasure = require('treasure-class');
 var Consumable = require('consumable-class');
 var Accountant = require('accountant-class');
+var Announcer = require('announcer-class');
 var NumberSelect = require('number-select-class');
 
-
 var accountant = new Accountant(config);
-
+var announcer = new Announcer();
 
 treasureItems.forEach(function(treasureItem){
   treasureItem.instance = new Treasure(
     treasureItem,
     accountant,
+    announcer,
     config
   ).render();
 });
@@ -28,6 +29,7 @@ new Consumable({
     initialPrice: 750
   },
   accountant,
+  announcer,
   config
 ).render();
 
@@ -37,13 +39,14 @@ new Consumable({
     initialPrice: 750
   },
   accountant,
+  announcer,
   config
 ).render();
 
 
 
-var areaSelect = new NumberSelect(0);
-var levelSelect = new NumberSelect(0);
+var areaSelect = new NumberSelect({id: 'area', number: 1}, announcer);
+var levelSelect = new NumberSelect({id: 'level', number: 1}, announcer);
 areaSelect.render(config.areaSelectElement);
 levelSelect.render(config.levelSelectElement);
 
